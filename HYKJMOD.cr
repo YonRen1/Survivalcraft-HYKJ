82123fc98a42764028d81f59cd20ea943d4ae043
永仁 <14883238+YonRen@user.noreply.gitee.com>
2025-01-15 03:34:12 +0000

    新增
    Signed-off-by: 永仁 <14883238+YonRen@user.noreply.gitee.com>

---
+++ HYKJMOD.cr
@@ -0,0 +1,19 @@
+<mod>
+  <Recipes>
+      <Recipe Result="plant_fiberBlock" ResultCount="2" RequiredHeatLevel="0" RequiredPlayerLevel="0" a="tallgrass" Description="合成物品" >
+         "a"
+         "a"
+      </Recipe>
+      <Recipe Result="straw_ropeBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="0" a="tallgrass" Description="合成物品" >
+         "a"
+         "a"
+         "a"
+      </Recipe>
+      <Recipe Result="hemp_ropeBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="0" a="leather" Description="合成物品" >
+         "a"
+         "a"
+         "a"
+      </Recipe>
+
+  </Recipes>
+</mod>
